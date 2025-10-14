c85e36addd955598a0fd5d5a40d811e08f75e99f
1ahmedphd <ak3242716@gmail.com>
2025-10-14 11:22:02 +0800

    started the document class

---
+++ src/oodj/document.java
@@ -0,0 +1,42 @@
+//make document class
+package oodj;
+public class document {
+    private String title;
+    private String content;
+    
+    //constructor
+    public document(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+    
+    //getters and setters
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getContent() {
+        return content;
+    }
+    
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void CreateTable() {
+        // TODO Auto-generated method stub
+        
+    }
+    public void createProgress() {
+        // TODO Auto-generated method stub
+        
+    }
+    public void ToDocument() {
+        // TODO Auto-generated method stub
+        
+    }
+}
