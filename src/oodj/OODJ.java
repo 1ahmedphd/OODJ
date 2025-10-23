ff50d5223a8a2c3ae6ef43de94bf9c392fe3ce96
Yusuf Arslan <a.online2006@gmail.com>
2025-10-23 17:17:05 +0800

    learning commit

--- src/oodj/OODJ.java
+++ src/oodj/OODJ.java
@@ -14,7 +14,7 @@ public class OODJ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // test
     }
     
 }
