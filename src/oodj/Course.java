cd81fee92a28b11374080f6952f4094b63ccb426
Yusuf Arslan <a.online2006@gmail.com>
2025-10-25 17:35:04 +0800

    Create Attributes and getters for Course Class ( initial attributes )

--- src/oodj/Course.java
+++ src/oodj/Course.java
@@ -1,5 +1,12 @@
 package oodj;
 
 public class Course {
-    
+    private String courseID ; 
+    private String courseName ; 
+    private int credit ; 
+    // ID , Name and credit will only have Getters because within the scope of this system Courses are not Created or generated so 
+    // this information only gets fetched from the text file that is storing info about Courses 
+    public String getCourseID () {return courseID ; }
+    public String getCourseName () {return courseName ; }
+    public int getcredit () {return credit ; }
 }
