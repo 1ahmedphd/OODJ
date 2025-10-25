43de1a833e7685f23a22f627de0aeca1805c94c8
Yusuf Arslan <a.online2006@gmail.com>
2025-10-25 16:23:15 +0800

    Added the Setters and getters for the recoveryplanMetadata class

--- src/oodj/RecoveryPlanMetadata.java
+++ src/oodj/RecoveryPlanMetadata.java
@@ -10,11 +10,59 @@ public class RecoveryPlanMetadata {
     private String recordID;
     private String planID;
     private String studentID;
-    private int attemptNO ; 
+    private int attemptNo ; 
     private String studentName;
     private String courseTitle;
     private List<String> failedComponents;
     private LocalDate createdDate; 
     private PlanStatus planStatus;
-    private AcademicOfficer createdBy ; 
+    private AcademicOfficer createdBy ;
+    // Getter only (auto-generated)
+    public String getRecordID() {
+        return recordID;
+    }
+
+    // Getter only (fetched from RecoveryPlan)
+    public String getPlanID() {
+        return planID;
+    }
+
+    // Getter only (fetched from Student object)
+    public String getStudentID() {
+        return studentID;
+    }
+    // Getter only (auto-fetched from the Academic records of students): 2nd attemp only need to repeat the failed component, 3rd attempt repeat all components of the module 
+    public int getAttemptNo() {
+        return attemptNo;
+    }
+    // Getter only (fetched from Student object)
+    public String getStudentName() {
+        return studentName;
+    }
+
+// Getter only (fetched from course record in text file)
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+// has a getter only as the failed components are automatically fetched from the academic record of the students which will be stored in a text file 
+    public List<String> getFailedComponents() 
+    {
+        return failedComponents;
+    }
+// Getter only as The Academic Officer who created the Recovery plan will be fetche From The Session Class 
+    public AcademicOfficer getCreatedBy()
+    {
+        return createdBy;
+    }
+
+    // Getter & Setter (updated based on plan progress has 2 possible value  Active / Completed 
+    public PlanStatus getPlanStatus() {
+        return planStatus;
+    }
+
+    public void setPlanStatus(PlanStatus planStatus) {
+        this.planStatus = planStatus;
+    }
+    
 }
+
