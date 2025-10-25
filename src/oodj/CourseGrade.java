0f89dadba7bbc958585257fb0927e7d951f1b4f6
Yusuf Arslan <a.online2006@gmail.com>
2025-10-25 18:09:53 +0800

    Created the getters setters for the fields in CourseGrade class ( course , numericGrade )

--- src/oodj/CourseGrade.java
+++ src/oodj/CourseGrade.java
@@ -3,11 +3,24 @@ package oodj;
 public class CourseGrade {
     private Course course ; 
     private int numericGrade ; 
+    public Course getCourse() {return course ;}
+    public void setCourse(Course course ) {this.course = course;}
+    // Getter & Setter for numericGrade
+    public int getNumericGrade() {
+        return numericGrade;
+    }
+
+    public void setNumericGrade(int numericGrade) {
+        this.numericGrade = numericGrade;
+    }
+
     public CourseGrade(Course course , int numericGrade) 
     {
         this.course = course ; 
         this.numericGrade = numericGrade ; 
     }
+
+
     // Getting the Grade Letter without Storing it explicity
     public String returnLetterGrade() {
         if (numericGrade >= 80)
