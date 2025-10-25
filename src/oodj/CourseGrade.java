c4cefc7500585afff4ad3b2303e14862d3773df0
Yusuf Arslan <a.online2006@gmail.com>
2025-10-25 18:06:43 +0800

    Created The Coruse Grade Class and some its attributtes , Constructor and a method that returns letter Grade

---
+++ src/oodj/CourseGrade.java
@@ -0,0 +1,36 @@
+package oodj;
+
+public class CourseGrade {
+    private Course course ; 
+    private int numericGrade ; 
+    public CourseGrade(Course course , int numericGrade) 
+    {
+        this.course = course ; 
+        this.numericGrade = numericGrade ; 
+    }
+    // Getting the Grade Letter without Storing it explicity
+    public String returnLetterGrade() {
+        if (numericGrade >= 80)
+            return "A+";
+        else if (numericGrade >= 75)
+            return "A";
+        else if (numericGrade >= 70)
+            return "B+";
+        else if (numericGrade >= 65)
+            return "B";
+        else if (numericGrade >= 60)
+            return "C+";
+        else if (numericGrade >= 55)
+            return "C";
+        else if (numericGrade >= 50)
+            return "C-";
+        else if (numericGrade >= 40)
+            return "D";
+        else if (numericGrade >= 30)
+            return "F+";
+        else if (numericGrade >= 20)
+            return "F";
+        else
+            return "F-";
+    }
+}
